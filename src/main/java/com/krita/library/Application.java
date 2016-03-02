45c22c13881a0ebe3f4b0af61a8642d4def7d022
soundar <soundarin@gmail.com>
2016-03-02 00:16:13 +0000

    Library ModuleChanges

---
+++ src/main/java/com/krita/library/Application.java
@@ -0,0 +1,60 @@
+package com.krita.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * Created by soundar on 22/02/2016.
+ */
+@SpringBootApplication
+public class Application  { //implements CommandLineRunner {
+
+//    @Autowired
+//    private CustomerRepository repository;
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        repository.deleteAll();
+//
+//        // save a couple of customers
+//        repository.save(new Customer("Alice", "Smith"));
+//        repository.save(new Customer("Bob", "Smith"));
+//
+//        // fetch all customers
+//        System.out.println("Customers found with findAll():");
+//        System.out.println("-------------------------------");
+//        for (Customer customer : repository.findAll()) {
+//            System.out.println(customer);
+//        }
+//        System.out.println();
+//
+//        // fetch an individual customer
+//        System.out.println("Customer found with findByFirstName('Alice'):");
+//        System.out.println("--------------------------------");
+//        System.out.println(repository.findByFirstName("Alice"));
+//
+//        System.out.println("Customers found with findByLastName('Smith'):");
+//        System.out.println("--------------------------------");
+//        for (Customer customer : repository.findByLastName("Smith")) {
+//            System.out.println(customer);
+//        }
+//
+//
+//    }
+}
